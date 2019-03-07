dc684c5c3ea865eedda0d5972336a87714831be6
vibhorj <vibhorj@hotmail.com>
2019-03-07 17:02:55 +1300

    streaming

--- gcp/bq/query.py
+++
@@ -1,8 +0,0 @@
-from google.cloud import bigquery
-bq = bigquery.Client(project = {{PROJECT_ID}}})
-#import os
-#sprint(os.environ['GOOGLE_APPLICATION_CREDENTIALS'])
-
-sql = 'SELECT * FROM `api.dummy_data`'
-job = bq.query(sql) # bigquery.jobs.insert
-x = job.done()
